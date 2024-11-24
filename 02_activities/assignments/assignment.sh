7c05c2ef2d790deb189ad074b319d6420d1902c8
teresa-le <teresale8@gmail.com>
2024-11-24 18:30:54 -0500

    Answer Q 7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -63,11 +63,9 @@ cp *event*.log ./data/raw ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
-
+rm *ipaddr* ./data/raw ./data/processed/user_logs
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
-
 
 ###########################################
 
