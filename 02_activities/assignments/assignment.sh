c436aefbd582fc7c4259c4d93b22b5c204081cb7
teresa-le <teresale8@gmail.com>
2024-12-01 12:00:27 -0500

    Update to list all files in subdirectories for part 8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -62,7 +62,7 @@ find ./data/raw ./data/processed/user_logs -type f -name "*ipaddr*" -exec rm {} 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
 touch ./data/inventory.txt 
-ls ./data/processed >> ./data/inventory.txt
+ls -R ./data/processed >> ./data/inventory.txt
 
 ###########################################
 
